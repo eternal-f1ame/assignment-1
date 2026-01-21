26eca6f5b43a8e8ed1f0b82f5bfa62735f20019d
Aaditya Baranwal <thunder.tanay.1907@gmail.com>
2026-01-20 22:51:00 -0500

    refactor: enforce strict number types for fibonacci calculation

--- src/fib.ts
+++ src/fib.ts
@@ -1,13 +1,12 @@
+// util function that computes the fibonacci numbers
 export default function fibonacci(n: number): number {
   if (n < 0) {
     return -1;
-  } else if (n == 0) {
+  } else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
   }
 
-  // Because 'n' is now a number, the '+' operator is safe
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
- // EOF
